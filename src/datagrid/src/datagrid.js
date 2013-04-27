a6a869214d9f04f023985af29e281007b5f61d15
mengjiaqi <mengjq08@126.com>
2013-04-27 16:52:58 +0800

    数据表格初始化

--- src/datagrid/src/datagrid.js
+++ src/datagrid/src/datagrid.js
@@ -1,6 +1,7 @@
-/**
- * Name:
- * Desc: .
- * User: mengjq < mailto:mengjq@asiainfo-linkage.com >
- * Time: 13-4-27 下午3:46
- */
+define(function(require, exports, module) {
+
+    var datagrid;
+
+    module.exports = datagrid;
+});
+
---
+++ src/datagrid/tests/datagrid-spec.js
@@ -0,0 +1,13 @@
+define(function(require) {
+
+    var datagrid = require('../src/datagrid');
+
+    describe('datagrid', function() {
+
+        it('normal usage', function() {
+
+        });
+    });
+
+});
+
